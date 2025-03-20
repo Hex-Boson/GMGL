8bdd9f40da62e639618055eb09b32bc3163e62e5
Hex-Boson <milan.steiner@posteo.de>
2025-03-20 18:35:18 +0100

    eingabefelder hinzugefügt

--- app.py
+++ app.py
@@ -18,7 +18,16 @@ def neuesSpielbrett(hoehe,breite):
     spielbrett=sb.Spielbrett(hoehe,breite)
 def zufaelligFuellen():
     spielbrett.zufaellig_fuellen()
-
+def getHoehe():
+    if not eingabeHoehe.getint()>=0:
+        return 10
+    else:
+        return eingabeHoehe.getint()
+def getBreite():
+    if not eingabeBreite.getint()>=0:
+        return 20
+    else:
+        return eingabeBreite.getint()
 #Spiel
 ueberschrift=tk.Label(root,text="GMGL")
 ueberschrift.grid()
@@ -26,12 +35,17 @@ ueberschrift.grid()
 #Display
 screen=tk.Canvas(root,width=960,height=540)
 
+#Eingabefeld für Spielbrettgröße
+eingabeHoehe=tk.Entry(root,width=15)
+eingabeHoehe.grid(column=0,row=1)
+eingabeBreite=tk.Entry(root,width=15)
+eingabeBreite.grid(column=2,row=1)
+
 #button für neues Spielbrett
-neuesSpielbrettButton = tk.Button(
-                   text="neues Spielbrett",command=neuesSpielbrett(10,20))
-neuesSpielbrettButton.grid()
+neuesSpielbrettButton = tk.Button(text="neues Spielbrett",command=neuesSpielbrett(10,20))
+neuesSpielbrettButton.grid(column=4,row=1)
 #button für zufälliges erschaffen von Leben
 zufaelligFuellenButton=tk.Button(text="zufällig füllen",command=zufaelligFuellen())
 zufaelligFuellenButton.grid()
-
+print(eingabeBreite.get())
 root.mainloop()
