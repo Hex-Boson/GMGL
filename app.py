cddd75ebfb4fec97438d569d18e7e5940623637d
Hex-Boson <milan.steiner@posteo.de>
2025-03-20 21:36:49 +0100

    fixed input for boardsize, rearranged folder structure

--- app.py
+++ app.py
@@ -11,41 +11,37 @@ root.title("GMGL")
 root.geometry("1280x720")
 
 #Variablen
-spieler=[]
+textfeldHoehe=tk.IntVar()
+textfeldBreite=tk.IntVar()
+
 #Funktionen
 def neuesSpielbrett(hoehe,breite):
+    print(textfeldHoehe.get(),textfeldBreite.get())
     global spielbrett
     spielbrett=sb.Spielbrett(hoehe,breite)
 def zufaelligFuellen():
     spielbrett.zufaellig_fuellen()
-def getHoehe():
-    if not eingabeHoehe.getint()>=0:
-        return 10
-    else:
-        return eingabeHoehe.getint()
-def getBreite():
-    if not eingabeBreite.getint()>=0:
-        return 20
-    else:
-        return eingabeBreite.getint()
-#Spiel
+
 ueberschrift=tk.Label(root,text="GMGL")
-ueberschrift.grid()
+ueberschrift.grid(column=0,row=0)
 
 #Display
-screen=tk.Canvas(root,width=960,height=540)
+screen=tk.Canvas(root,width=960,height=540,bg="black")
+screen.grid(row=2,columnspan=40)
+
 
 #Eingabefeld für Spielbrettgröße
-eingabeHoehe=tk.Entry(root,width=15)
-eingabeHoehe.grid(column=0,row=1)
-eingabeBreite=tk.Entry(root,width=15)
-eingabeBreite.grid(column=2,row=1)
+eingabeHoehe=tk.Entry(root,width=5,textvariable=textfeldHoehe)
+eingabeHoehe.grid(column=0,row=4)
+eingabeBreite=tk.Entry(root,width=5,textvariable=textfeldBreite)
+eingabeBreite.grid(column=1,row=4)
 
+#Buttons
 #button für neues Spielbrett
-neuesSpielbrettButton = tk.Button(text="neues Spielbrett",command=neuesSpielbrett(10,20))
-neuesSpielbrettButton.grid(column=4,row=1)
+neuesSpielbrettButton = tk.Button(text="neues Spielbrett",command=neuesSpielbrett(textfeldHoehe.get(),textfeldBreite.get()))
+neuesSpielbrettButton.grid(column=2,row=4)
 #button für zufälliges erschaffen von Leben
 zufaelligFuellenButton=tk.Button(text="zufällig füllen",command=zufaelligFuellen())
-zufaelligFuellenButton.grid()
-print(eingabeBreite.get())
+zufaelligFuellenButton.grid(column=3,row=4)
+
 root.mainloop()
